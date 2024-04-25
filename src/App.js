22f8e80c3cf3513189dfecf4b81893a7b3e41727
Allovallo <30566134+Allovallo@users.noreply.github.com>
2024-04-25 15:29:43 +0300

    files updated

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from 'react';
+import ProductReviewForm from 'components/ProductReviewForm/ProductReviewForm';
 import LoginForm from 'components/LoginForm/LoginForm';
 
 function App() {
   return (
     <div>
-      <LoginForm />
+      {/* <LoginForm /> */}
+      <ProductReviewForm />
     </div>
   );
 }
